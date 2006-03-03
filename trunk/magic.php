67b5ad72315cc1f37aa280f906ba85ca60733317
vegarg <vegarg@c4638b12-f00d-0410-a403-cb5a613e8f29>
2006-03-03 16:33:42 +0000

    M    magic.php

--- trunk/magic.php
+++ trunk/magic.php
@@ -1,3 +1,28 @@
 <?php
 // this script will perform all the magic - viz., adding new entries/groups/etc., editing them, deleting them, and all other dirty work
+
+if (isset($GET['action'])) {
+	$action = $GET['action'];
+	$id = $GET['id'];
+} elseif (isset($POST['action'])) {
+	$action = $POST['action'];
+	$id = $POST['id'];
+} else {
+	// no action requested
+	die("Error: no action requested.\n");
+}
+switch($action) {
+	case 'add':
+		// write post to db
+		break;
+	case 'edit':
+		// open editing window (edit.php) (with original post contents where applicable)
+		break;
+	case 'del':
+		// delete post from db
+		break;
+	default:
+		// report error
+		die ("Unknown action.\n");
+}
 ?>
